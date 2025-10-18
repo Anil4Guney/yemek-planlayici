ff2a8190f0c78d774fd848ad5dd8fb255c9154e1
Anıl Güney <147307816+Anil4Guney@users.noreply.github.com>
2025-10-18 13:29:14 +0300

    a

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAxW4Z8LAChESzBkfIXrftoAYzRbIa1xV4",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "yemek-planlayici.firebaseapp.com",
   projectId: "yemek-planlayici",
   storageBucket: "yemek-planlayici.firebasestorage.app",
